2c3fe6ca96a21c45e1ca491ed86d9f12896ff78b
rachel.wu <119239275@qq.com>
2026-03-09 09:27:52 +0800

    Adding Skeleton code

--- introDs/src/main/java/SymbolTable/BST.java
+++ introDs/src/main/java/SymbolTable/BST.java
@@ -1,46 +1,18 @@
 package SymbolTable;
 
 public class BST <Key extends Comparable<Key>, Value> {
-    private  class Node{
-        private Key key;
-        private Value value;
-        private Node left;
-        private Node right;
-        public Node(Key key, Value value){
-            this.key = key;
-            this.value = value;
-        }
-    }
 
-    private Node root;
 
     public BST(){}
     public void put(Key key, Value value){
 
-        put(root, key,value);
 
     }
 
-    private Node put(Node node , Key key, Value value){
-        if( node == null) node = new Node(key, value);
 
-        int cmp = key.compareTo(node.key);
-        if(cmp == 0) node.value = value;
-        else if( cmp < 0) put(node.left, key,value);
-        else put (node.right, key,value);
-        return node;
-    }
 
     public Value get(Key key){
-        Node x = root;
-        while(x != null){
-            int cmp = key.compareTo(x.key);
-            if(cmp == 0) return x.value;
-            else if(cmp < 0 ) x = x.left;
-            else x = x.right;
-
-        }
-        return null;
+        return  null;
     }
 
 
@@ -55,6 +27,27 @@ public class BST <Key extends Comparable<Key>, Value> {
     }
 
     public boolean isEmpty(){
-        return root == null;
+       return false;
+    }
+
+    public Key floor(Key key){
+        return null;
+    }
+
+    public Key ceiling(Key key){
+        return null;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public int rank(Key key){
+        return -1;
+
+    }
+
+    public Key select(int k){
+        return null;
     }
 }
